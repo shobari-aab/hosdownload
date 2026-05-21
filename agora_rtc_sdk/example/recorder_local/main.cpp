c60337b51f60f00739691b4a1b1bfd8a47b40249
Shobari <shobari@asuransiastra.com>
2026-05-21 16:58:55 +0700

    fix stop record

--- agora_rtc_sdk/example/recorder_local/main.cpp
+++ agora_rtc_sdk/example/recorder_local/main.cpp
@@ -356,6 +356,7 @@ int main(int argc, char * const argv[])
   while (!g_bSignalStop) {
     usleep(100*1000);
     int userCount = eventHandler->getUidsCount();
+    AG_LOG(INFO, "Current joined user count: %d", userCount);
     if (userCount == 0) {
       idleLimitMs -= 100;
       if (idleLimitMs <= 0) {
@@ -375,9 +376,12 @@ int main(int argc, char * const argv[])
     recorder->unsubscribeAllAudio();
     recorder->unsubscribeAllVideo();
   }
+  AG_LOG(INFO, "isMix=%d, about to stop", config.isMix);
   if(config.isMix){
+    AG_LOG(INFO, "Stopping recording with stopRecording()...");
     recorder->stopRecording();
   } else {
+    AG_LOG(INFO, "Stopping individual user recording with stopSingleRecordingByUid()...");
     std::set<std::string> uids = eventHandler->getUids();
     for(auto& uid : uids){
       recorder->stopSingleRecordingByUid(uid.c_str());
